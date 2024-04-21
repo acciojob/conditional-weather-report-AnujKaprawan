9dffb3b75c803a1b13a145e53c0394a211b7d09a
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-04-21 20:51:50 +0530

    added code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
-const App = () => {
+const WeatherApp = ({ weather }) => {
+  const { temperature, conditions } = weather;
+  const threshold = 20;
+
+  const temperatureColor = temperature > threshold ? "red" : "blue";
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Current Weather</h2>
+      <p>
+        Temperature:{" "}
+        <span style={{ color: temperatureColor }}>{temperature}</span>
+      </p>
+      <p>Conditions: {conditions}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const weatherInput = { temperature: 25, conditions: "Sunny" };
+
+  return (
+    <div>
+      <WeatherApp weather={weatherInput} />
+    </div>
+  );
+};
+export default App;
